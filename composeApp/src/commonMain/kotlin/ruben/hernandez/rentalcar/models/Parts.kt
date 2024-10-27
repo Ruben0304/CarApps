05baad2e08d7e9b8526957903f29ac77b4ce0c51
Fabian1820 <149929094+Fabian1820@users.noreply.github.com>
2024-10-27 15:31:31 -0400

    vml

---
+++ composeApp/src/commonMain/kotlin/ruben/hernandez/rentalcar/models/Parts.kt
@@ -0,0 +1,10 @@
+package ruben.hernandez.rentalcar.models
+
+data class Parts(
+    val id: String?,
+    val tipo: String?,
+    val modelo: String?,
+    val precio: Int?,
+    val uso: String?,
+    val cantidad: Int?
+)
